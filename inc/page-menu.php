976d6c3cadc029ef5e5855a67073068d2215b49d
amine_herradi <amineherradi@gmail.com>
2017-01-04 12:18:51 +0100

    Adding include page-menu

---
+++ inc/page-menu.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div class="page__menu">
+        <div class="page__menu--item"><a href="informations-personnelles.php"><i class="icon item-1"></i><span class="item__text">Informations personnelles</span></a></div>
+        <div class="page__menu--item"><a href="ma-consommation.php"><i class="icon item-2"></i><span class="item__text">Ma consommation</span></a></div>
+        <div class="page__menu--item"><a href="cartes-enregistrees.php"><i class="icon item-3"></i><span class="item__text">Cartes enregistrées</span></a></div>
+        <div class="page__menu--item"><a href="mes-messages.php"><i class="icon item-4"></i><span class="item__text">Mes messages (3)</span></a></div>
+        <div class="page__menu--item"><a href="historique-chat.php"><i class="icon item-5"></i><span class="item__text">Historique des conversations</span></a></div>
+        <div class="page__menu--item"><a href="factures.php"><i class="icon item-6"></i><span class="item__text">Factures</span></a></div>
+      </div>
